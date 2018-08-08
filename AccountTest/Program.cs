33c02ea037f3ed964d424123004e16aceddfe351
Praveen Nunna <praveen.nunna@hotmail.com>
2018-08-08 08:32:38 +0200

    Basic impl

---
+++ AccountTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context(new StateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Account
+    {
+        public string HolderName { get; set; }
+
+        public AccountState accountState;
+
+        private int Balance { get; set; }
+       
+        public Account(string holderName)
+        {
+            this.HolderName = holderName;
+            this.accountState = new NotVerifedState();
+        }
+    }
+    public abstract class AccountState
+    {
+        public int Balance { get; set; }
+
+        public bool IsVerified { get; set; }
+        public abstract Account account { get; set; }
+
+        public abstract void Handle(Account account);
+
+        public abstract void UpdateState();
+    }
+
+    public class NotVerifedState : AccountState
+    {
+        public override Account account { get; set; }
+
+        public NotVerifedState()
+        {
+
+        }
+        public override void UpdateState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Handle(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //public class VerifiedState : AccountState
+    //{
+
+    //}
+
+    //public class ClosedState : AccountState
+    //{
+
+    //}
+}
---
+++ AccountTest/StateDesignPattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    public class Context
+    {
+        private State _state;
+
+        public State State
+        {
+            get
+            {
+                return _state;
+            }
+            set { _state = value; }
+        }
+        public Context(State state)
+        {
+            this.State = state;
+        }
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    public class StateA : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("This is State A");
+            context.State = new StateB();
+        }
+    }
+
+    public class StateB : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("This is State B");
+            context.State = new StateA();
+        }
+    }
+}
